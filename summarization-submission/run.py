a16f537d04e3b0f11814e353464eeedc026f5116
Your Name <you@example.com>
2024-06-12 18:30:58 +0200

    Update summarization model

--- summarization-submission/run.py
+++ summarization-submission/run.py
@@ -5,10 +5,10 @@ from transformers import pipeline
 from tira.rest_api_client import Client
 from tira.third_party_integrations import get_output_directory
 
-def generate_summary(model, text, max_length=1024):
-    inputs = model.tokenizer(text, truncation=True, max_length=max_length, return_tensors="pt")
-    output_ids = model.model.generate(**inputs, max_length=130, min_length=30, do_sample=False)
-    summary = model.tokenizer.batch_decode(output_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)[0]
+def generate_summary(summarizer, text, max_new_tokens=130):
+    if len(text) > 1024:
+        text = text[:1024]
+    summary = summarizer(text, max_new_tokens=max_new_tokens, min_length=30, do_sample=False)[0]['summary_text']
     return summary
 
 if __name__ == "__main__":
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     df = tira.pd.inputs("nlpbuw-fsu-sose-24", "summarization-validation-20240530-training").set_index("id")
 
     # Initialize the summarization pipeline
-    summarizer = pipeline(task="summarization", model="facebook/bart-large-cnn")
+    summarizer = pipeline(task="summarization", model="sshleifer/distilbart-cnn-12-6")
 
     # Generate summaries
     df["summary"] = df["story"].apply(lambda text: generate_summary(summarizer, text))
--- summarization-submission/train.py
+++ summarization-submission/train.py
@@ -2,16 +2,19 @@ import json
 from transformers import pipeline
 
 # Initialize the summarization pipeline
-summarizer = pipeline("summarization")
+summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
-# Load the test set articles
+# Load the test set articles    
 with open("test_text.jsonl", "r") as f:
     test_articles = [json.loads(line) for line in f]
 
 # Generate summaries
 predictions = []
 for article in test_articles:
-    summary = summarizer(article['story'], max_length=130, min_length=30, do_sample=False)[0]['summary_text']
+    text = article['story']
+    if len(text) > 1024:
+        text = text[:1024]
+    summary = summarizer(text, max_new_tokens=130, min_length=30, do_sample=False)[0]['summary_text']
     predictions.append({
         "id": article["id"],
         "summary": summary
